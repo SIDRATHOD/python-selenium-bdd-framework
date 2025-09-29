584a48eafc2b86da1492a7bdf625f347061fe691
SIDRATHOD <siddharthrathod02@gmail.com>
2025-09-29 12:21:32 +0530

    Second commit, updated locator to test the self healing locator

--- locator/profile_locators.py
+++ locator/profile_locators.py
@@ -2,5 +2,5 @@ from selenium.webdriver.common.by import By
 
 class ProfileLocators:
     FILE_INPUT = (By.ID, "profile-pic")
-    UPLOAD_BUTTON = (By.XPATH, "//button[@onclick='uploadProfilePic()']")
+    UPLOAD_BUTTON = (By.XPATH, "//button[onclick='uploadProfilePic()']")
     UPLOAD_MSG = (By.ID, "upload-msg")
